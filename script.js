74b506432c412d942c7b68c6a5633ae5032d27e1
Dzmitry Datskevich <dda.enigmer@gmail.com>
2020-10-07 18:43:23 +0300

    fix: virtual Btm

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 let classErm = document.getElementById("hidden-calc");
 let bodyStyle = document.body;
 
+let numbers = document.querySelectorAll(".number");
+let operations = document.querySelectorAll(".operations");
+let clearBtns = document.querySelectorAll(".clear-btn");
+let decimalBtn = document.getElementById("decimal");
+let result = document.getElementById("result");
+let MemoryCurrentNumber = 0;
+let MemoryNewNumber = false;
+let MemoryPendingOperation = "";
+
 let display = document.querySelector("#display-result");
 let displayMemory = document.querySelector("#display-memory");
 let operandFirst = "";
@@ -9,13 +18,96 @@ let operandSecond = "";
 let answer = "";
 let virtualBotton = "";
 
-/**
- * Возвращает x, возведённое в n-ную степень.
- *
- * @param {number} x Возводимое в степень число.
- * @param {number} n Степень, должна быть натуральным числом.
- * @return {number} x, возведённое в n-ную степень.
- */
+// код из примера
+for (var i = 0; i < numbers.length; i++) {
+  var number = numbers[i];
+  number.addEventListener("click", function (e) {
+    numberPress(e.target.textContent);
+  });
+}
+
+for (var i = 0; i < operations.length; i++) {
+  var operationBtn = operations[i];
+  operationBtn.addEventListener("click", function (e) {
+    operationPress(e.target.textContent);
+  });
+}
+
+for (var i = 0; i < clearBtns.length; i++) {
+  var clearBtn = clearBtns[i];
+  clearBtn.addEventListener("click", function (e) {
+    clear(e.target.textContent);
+  });
+}
+
+decimalBtn.addEventListener("click", decimal);
+
+result.addEventListener("click", function (e) {
+  console.log("Клик по result");
+});
+
+function numberPress(number) {
+  if (MemoryNewNumber) {
+    display.value = number;
+    MemoryNewNumber = false;
+  } else {
+    if (display.value === "0") {
+      display.value = number;
+    } else {
+      display.value += number;
+    }
+  }
+}
+
+function operationPress(op) {
+  localOperationMemory = display.value;
+
+  if (MemoryNewNumber && MemoryPendingOperation !== "=") {
+    display.value = MemoryCurrentNumber;
+  } else {
+    MemoryNewNumber = true;
+    if (MemoryPendingOperation === "+") {
+      MemoryCurrentNumber += +localOperationMemory;
+    } else if (MemoryPendingOperation === "-") {
+      MemoryCurrentNumber -= +localOperationMemory;
+    } else if (MemoryPendingOperation === "*") {
+      MemoryCurrentNumber *= +localOperationMemory;
+    } else if (MemoryPendingOperation === "/") {
+      MemoryCurrentNumber /= +localOperationMemory;
+    } else {
+      MemoryCurrentNumber = +localOperationMemory;
+    }
+    display.value = MemoryCurrentNumber;
+    MemoryPendingOperation = op;
+  }
+}
+
+function decimal(argument) {
+  var localDecimalMemory = display.value;
+
+  if (MemoryNewNumber) {
+    localDecimalMemory = "0.";
+    MemoryNewNumber = false;
+  } else {
+    if (localDecimalMemory.indexOf(".") === -1) {
+      localDecimalMemory += ".";
+    }
+  }
+  display.value = localDecimalMemory;
+  console.log("Клик по ");
+}
+
+function clear(id) {
+  if (id === "ce") {
+    display.value = "0"; // здесь строка? или число?
+    MemoryNewNumber = true;
+  } else if (id === "c") {
+    display.value = "0";
+    MemoryNewNumber = true;
+    (MemoryCurrentNumber = 0), (MemoryPendingOperation = "");
+  }
+}
+// выше код из примера
 
 function numPress(e) {
   // функция обработчик листнера нажатия клавиш Num
@@ -191,7 +283,7 @@ function operatorChange(o) {
 
 function displayEveryData(f, o, s) {
   if (bodyStyle.className === "erm-math-class") {
-    displayMemory.value = " Лукашеночка победил по ашчушчениям ";
+    displayMemory.value = "  Лукашеночка победил по ашчушчениям ";
     display.value = " и набрал 80,1 % голосов";
   } else {
     displaySub = operandFirst + " " + operator + " " + operandSecond + "";
@@ -240,110 +332,3 @@ function erm() {
   displayMemory.value = "";
   display.value = "";
 }
-
-/*
-
-var numbers = document.querySelectorAll(".number"),
-    operations = document.querySelectorAll(".operator"),
-    clearBtns = document.querySelectorAll(".clear-btn"),
-    decimalBtn = document.getElementById("decimal"),
-    result = document.getElementById("result"),
-    display = document.getElementById("display"),
-    MemoryCurrentNumber = 0,
-    MemoryNewNumber = false,
-    MemoryPendingOperation = "";
-
-
-
-for(var i = 0; i < numbers.length; i++) {
-    var number = numbers[i];
-    number.addEventListener("click", function(e){
-        numberPress(e.target.textContent);
-    });
-};
-
-for(var i = 0; i < operations.length; i++) {
-    var operationBtn = operations[i];
-        operationBtn.addEventListener("click", function(e){
-            operationPress(e.target.textContent);
-        });
-    };
-
-for(var i = 0; i < clearBtns.length; i++) {
-    var clearBtn = clearBtns[i];
-        clearBtn.addEventListener("click", function(e){
-        clear(e.target.textContent);
-        });
-    };
-
-decimalBtn.addEventListener("click", decimal);
-
-result.addEventListener("click", function(e){
-    console.log("Клик по result")
-});
-
-    function numberPress(number) {
-        if(MemoryNewNumber) {
-            display.value = number;
-            MemoryNewNumber = false;
-        } else {
-            if(display.value === "0") {
-                display.value = number;
-            } else {
-                display.value += number;
-            };
-        };
-    };
-
-function operationPress(op) {
-        localOperationMemory = display.value;
-        
-        if(MemoryNewNumber && MemoryPendingOperation !== "=") {
-            display.value = MemoryCurrentNumber;
-        } else {
-            MemoryNewNumber = true;
-            if (MemoryPendingOperation === "+") {
-                MemoryCurrentNumber += +localOperationMemory;  
-            } else if (MemoryPendingOperation === "-") {
-                MemoryCurrentNumber -= +localOperationMemory;  
-            } else if (MemoryPendingOperation === "*") {
-                MemoryCurrentNumber *= +localOperationMemory;  
-            } else if (MemoryPendingOperation === "/") {
-                MemoryCurrentNumber /= +localOperationMemory;  
-            } else {
-                MemoryCurrentNumber = +localOperationMemory;  
-            }
-            display.value = MemoryCurrentNumber;
-            MemoryPendingOperation = op;
-        };
-        
-      }
-
-    function decimal(argument) {
-        var localDecimalMemory = display.value;
-        
-        if(MemoryNewNumber) {
-            localDecimalMemory = "0.";
-            MemoryNewNumber = false;
-        } else {
-            if(localDecimalMemory.indexOf(".") === -1) {
-                localDecimalMemory += "."
-            }
-        };
-        display.value = localDecimalMemory;
-        console.log("Клик по " )
-    };
-
-    function clear(id) {
-        if(id === "ce") {
-            display.value = "0" // здесь строка? или число?
-            MemoryNewNumber = true;
-        } else if(id === "c") {
-            display.value = "0" 
-            MemoryNewNumber = true;
-            MemoryCurrentNumber = 0,
-            MemoryPendingOperation = "";
-        }
-    };
-
-*/
