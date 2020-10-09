b06ebf800499b912ec0c878a898ac1fe3fdd6ffe
Dzmitry Datskevich <dda.enigmer@gmail.com>
2020-10-09 20:39:47 +0300

    fix: whole code was rework

--- script.js
+++ script.js
@@ -1,345 +1,206 @@
-let classErm = document.getElementById("hidden-calc");
-let bodyStyle = document.body;
+// обьявление переменных
+let buttons = document.querySelectorAll(".button");            // Выбор кнопок 
+let presedButton = 0;                                          // Переменная нажатой кнопки  
+let operandFirst = "";                                         // хранение первого операнда
+let operator = "";                                             // хранение оператора
+let operandSecond = "";                                        // хранение второго операнда
+let display = document.querySelector("#display-result");       // Выбор дисплэя ввода
+let displayMemory = document.querySelector("#display-memory"); // Выбор дисплэя истории
+let answer = "";                                               // хранение ответа
 
-// код из примера
-let numbers = document.querySelectorAll(".number");
-let operations = document.querySelectorAll(".operations");
-let clearBtns = document.querySelectorAll(".clear-btn");
-let decimalBtn = document.getElementById("decimal");
-let result = document.getElementById("result");
-let MemoryCurrentNumber = 0;
-let MemoryNewNumber = false;
-let MemoryPendingOperation = "";
-// выше код из примера
-
-let display = document.querySelector("#display-result");
-let displayMemory = document.querySelector("#display-memory");
-let operandFirst = "";
-let operator = "";
-let operandSecond = "";
-let answer = "";
-let virtualBotton = "";
-
-// код из примера
-for (var i = 0; i < numbers.length; i++) {
-  var number = numbers[i];
-  number.addEventListener("click", function (e) {
-    numberPress(e.target.textContent);
-  });
-}
-
-for (var i = 0; i < operations.length; i++) {
-  var operationBtn = operations[i];
-  operationBtn.addEventListener("click", function (e) {
-    operationPress(e.target.textContent);
-  });
-}
-
-for (var i = 0; i < clearBtns.length; i++) {
-  var clearBtn = clearBtns[i];
-  clearBtn.addEventListener("click", function (e) {
-    clear(e.target.textContent);
-  });
-}
-
-decimalBtn.addEventListener("click", decimal);
-
-result.addEventListener("click", function (e) {
-  console.log("Клик по result");
-});
-
-function numberPress(number) {
-  if (MemoryNewNumber) {
-    display.value = number;
-    MemoryNewNumber = false;
+function buttonPress(e) {
+  if (e.keyCode === undefined) {
+    presedButton = e.target.textContent; 
   } else {
-    if (display.value === "0") {
-      display.value = number;
-    } else {
-      display.value += number;
-    }
+    presedButton = e.keyCode;
   }
-}
-
-function operationPress(op) {
-  localOperationMemory = display.value;
-
-  if (bodyStyle.className === "erm-math-class") {
-    displayMemory.value = " Лукашеночка набрал 80,1 % голосов и";
-    display.value = "и дальше можете не считать ";
-  } else {
-    if (MemoryNewNumber && MemoryPendingOperation !== "=") {
-      display.value = MemoryCurrentNumber;
-    } else {
-      MemoryNewNumber = true;
-      if (MemoryPendingOperation === "+") {
-        MemoryCurrentNumber += +localOperationMemory;
-        operatorChange("+");
-        displayEveryData();
-      } else if (MemoryPendingOperation === "-") {
-        MemoryCurrentNumber -= +localOperationMemory;
-        operatorChange("-");
-        displayEveryData();
-      } else if (MemoryPendingOperation === "*") {
-        MemoryCurrentNumber *= +localOperationMemory;
-        operatorChange("*");
-        displayEveryData();
-      } else if (MemoryPendingOperation === "/") {
-        MemoryCurrentNumber /= +localOperationMemory;
-        operatorChange("/");
-        displayEveryData();
-      } else {
-        MemoryCurrentNumber = +localOperationMemory;
-      }
-      display.value = MemoryCurrentNumber;
-      MemoryPendingOperation = op;
-    }
-  }
-}
-
-function decimal(argument) {
-  let localDecimalMemory = display.value;
-
-  if (MemoryNewNumber) {
-    localDecimalMemory = "0.";
-    MemoryNewNumber = false;
-  } else {
-    if (localDecimalMemory.indexOf(".") === -1) {
-      localDecimalMemory += ".";
-    }
-  }
-  display.value = localDecimalMemory;
-}
-
-function clear(id) {
-  if (id === "ce") {
-    display.value = "0"; // здесь строка? или число?
-    MemoryNewNumber = true;
-  } else if (id === "c") {
-    display.value = "0";
-    MemoryNewNumber = true;
-    (MemoryCurrentNumber = 0), (MemoryPendingOperation = "");
-  }
-}
-// выше код из примера
-
-function numPress(e) {
-  // функция обработчик листнера нажатия клавиш Num
-  switch (e.keyCode) {
-    case 96: // если нажата клавиша Num0
-      console.log("нажата кнопка Num0");
+  // обработчик листнера нажатия клавиш
+  switch (presedButton) {
+    case "0":
+    case 96:                                  // если нажата клавиша 0
       pressNumber("0");
-      displayEveryData(); // вызывает решение и заносит его в display
-      break;
-    case 97: // если нажата клавиша Num1
-      console.log("нажата кнопка Num1");
+    break;
+    case "1":
+    case 97:                                  // если нажата клавиша 1
       pressNumber("1");
-      displayEveryData(); // вызывает решение и заносит его в display
-      break;
-    case 98: // если нажата клавиша Num2
-      console.log("нажата кнопка Num2");
+    break;
+    case "2":  
+    case 98:                                  // если нажата клавиша 2
       pressNumber("2");
-      displayEveryData(); // вызывает решение и заносит его в display
-      break;
-    case 99: // если нажата клавиша Num3
-      console.log("нажата кнопка Num3");
+    break;
+    case "3":
+    case 99:                                  // если нажата клавиша 3
       pressNumber("3");
-      displayEveryData(); // вызывает решение и заносит его в display
-      break;
-    case 100: // если нажата клавиша Num4
-      console.log("нажата кнопка Num4");
+    break;
+    case "4":
+    case 100:                                 // если нажата клавиша 4
       pressNumber("4");
-      displayEveryData(); // вызывает решение и заносит его в display
-      break;
-    case 101: // если нажата клавиша Num5
-      console.log("нажата кнопка Num5");
+    break;
+    case "5":
+    case 101:                                 // если нажата клавиша 5
       pressNumber("5");
-      displayEveryData(); // вызывает решение и заносит его в display
-      break;
-    case 102: // если нажата клавиша Num6
-      console.log("нажата кнопка Num6");
+    break;
+    case "6":
+    case 102:                                 // если нажата клавиша 6
       pressNumber("6");
-      displayEveryData(); // вызывает решение и заносит его в display
-      break;
-    case 103: // если нажата клавиша Num7
-      console.log("нажата кнопка Num7");
+    break;
+    case "7":
+    case 103:                                 // если нажата клавиша 7
       pressNumber("7");
-      displayEveryData(); // вызывает решение и заносит его в display
-      break;
-    case 104: // если нажата клавиша Num8
-      console.log("нажата кнопка Num8");
+    break;
+    case "8":
+    case 104:                                 // если нажата клавиша 8
       pressNumber("8");
-      displayEveryData(); // вызывает решение и заносит его в display
-      break;
-    case 105: // если нажата клавиша Num9
-      console.log("нажата кнопка Num9");
+    break;
+    case "9":
+    case 105:                                 // если нажата клавиша 9
       pressNumber("9");
-      displayEveryData(); // вызывает решение и заносит его в display
-      break;
-    case 106: // если нажата клавиша Num *
-      console.log("нажата кнопка Num *");
-      operatorChange("*");
-      displayEveryData(); // вызывает решение и заносит его в display
-      // сбросить operandSecond;
-      break;
-    case 107: // если нажата клавиша Num +
-      console.log("нажата кнопка Num +");
-      operatorChange("+");
-      displayEveryData(); // вызывает решение и заносит его в display
-      // сбросить operandSecond;
-      break;
-    case 111: // если нажата клавиша Num /
-      console.log("нажата кнопка Num /");
-      // сбросить operandSecond;
-      operatorChange("/");
-      displayEveryData(); // вызывает решение и заносит его в display
-      break;
-    case 109: // если нажата клавиша Num -
-      console.log("нажата кнопка Num -");
-      operatorChange("-");
-      displayEveryData(); // вызывает решение и заносит его в display
-      // сбросить operandSecond;
-      break;
-    case 108: // если нажата клавиша Num ,
-      console.log("нажата кнопка Num,");
-      decimalPress(",");
-      displayEveryData(); // вызывает решение и заносит его в display
-      break;
-    case 110: // если нажата клавиша Num , Для браузеров у которых другой Keycode
-      console.log("нажата кнопка Num ,");
-      decimalPress(",");
-      displayEveryData(); // вызывает решение и заносит его в display
-      break;
-    case 46: // если нажата клавиша delete
-      console.log("нажата кнопка delete");
+    break;
+    case "*":
+    case 106:                                 // если нажата клавиша  *
+      operatorChange("*")
+    break;
+    case "+":  
+    case 107:                                 // если нажата клавиша  +
+      operatorChange("+")
+    break;
+    case "/":
+    case 111:                                 // если нажата клавиша  /
+      operatorChange("/")
+    break;
+    case "-":
+    case 109:                                 // если нажата клавиша  -
+      operatorChange("-")
+    break;
+    case ",":
+    case 108:                                 // если нажата клавиша  ,
+    case 110:                                 // если нажата клавиша  , Для браузеров у которых другой Keycode
+      pressNumber(".");
+    break;
+    case "C":                                 // удаляет всё 
+    case 46:                                  // если нажата клавиша delete
       operandFirst = "";
       operator = "";
       operandSecond = "";
       answer = "";
-      display.value = "";
-      displayMemory.value = "";
-      break;
-    case 13: // если нажата клавиша enter
-      console.log("нажата кнопка enter");
-      operatorChange();
-      displayEveryData(); // вызывает решение и заносит его в display
-      answerResult();
-      break;
-    case 8: // если нажата клавиша backspace
-      console.log("нажата кнопка backspace");
-      // сбросить operandSecond;
-      break;
+      display.value = "0";
+      displayMemory.value = "0";
+    break;
+    case "=":
+    case 13:                                  // если нажата клавиша enter
+      answer =  eval(+operandFirst + operator + +operandSecond);
+      display.value = answer;
+    break;
+    case "CE":
+    case 8:                                   // если нажата клавиша backspace удаляет второй операнд
+      operandSecond = "";
+    break;
   }
+  displayMemory.value = operandFirst + "" + operator + "" + operandSecond + "";
+  display.value = answer;
 }
 
-function pressNumber(n) {
-  if (operator !== "") {
-    operandSecond += `${n}`;
-  } else {
-    operandFirst += `${n}`;
-  }
+// листнер - нажатия клавишь на Num-клавитуре
+addEventListener("keydown", buttonPress);
+
+// листнер  - нажатия клавишь на  виртуальной клавитуре
+for (let i = 0; i < buttons.length; i++) {
+  let number = buttons[i];
+  number.addEventListener("click", buttonPress);
 }
 
+// функция если оператор поменялся
 function operatorChange(o) {
   if (operandSecond === "") {
     switch (o) {
       case "+":
-        operator = "+"; // перезапиcываем operator
+        operator = "+";                       // перезапиcываем operator
         operandSecond = "";
         break;
       case "-":
-        operator = "-"; // перезапиcываем operator
+        operator = "-";                       // перезапиcываем operator
         operandSecond = "";
         break;
       case "/":
-        operator = "/"; // перезапиcываем operator
+        operator = "/";                       // перезапиcываем operator
         operandSecond = "";
         break;
       case "*":
-        operator = "*"; // перезапиcываем operator
+        operator = "*";                       // перезапиcываем operator
         operandSecond = "";
         break;
     }
   } else {
-    answerResult();
+    answer =  eval(+operandFirst + operator + +operandSecond);
     switch (o) {
       case "+":
-        operandFirst = answer;
-        operator = "+"; // перезапиcываем operator
-        operandSecond = "";
+        operandFirst = answer;                // заносим первый операнд в ответ 
+        operator = "+";                       // перезапиcываем operator
+        operandSecond = "";                   // обнуляем второй операнд
         break;
       case "-":
-        operandFirst = answer;
-        operator = "-"; // перезапиcываем operator
-        operandSecond = "";
+        operandFirst = answer;                // заносим первый операнд в ответ 
+        operator = "-";                       // перезапиcываем operator
+        operandSecond = "";                   // обнуляем второй операнд
         break;
       case "/":
-        operandFirst = answer;
-        operator = "/"; // перезапиcываем operator
-        operandSecond = "";
+        operandFirst = answer;                // заносим первый операнд в ответ 
+        operator = "/";                       // перезапиcываем operator
+        operandSecond = "";                   // обнуляем второй операнд
         break;
       case "*":
-        operandFirst = answer;
-        operator = "*"; // перезапиcываем operator
-        operandSecond = "";
+        operandFirst = answer;                // заносим первый операнд в ответ 
+        operator = "*";                       // перезапиcываем operator
+        operandSecond = "";                   // обнуляем второй операнд
         break;
     }
   }
-
-  // // заносит нажатия чисел в operandFirst
-  // // если нажат operator
-  // // заносит нажатия чисел в operandSecond
 }
 
-function displayEveryData(f, o, s) {
-  if (bodyStyle.className === "erm-math-class") {
-    displayMemory.value = " Лукашеночка набрал 80,1 % голосов и";
-    display.value = "и дальше можете не считать ";
-  } else {
-    displaySub = operandFirst + " " + operator + " " + operandSecond + "";
-    display.value = displaySub;
+// проверяет не пустой ли оператор и от условия заносит в первую или во вторую переменную
+function pressNumber(n) {
+
+  if (operator !== "") {                      // цифры вводятся во второй операнд
+    if (operandSecond === "0") {
+      if (n === ".")  {
+         if (operandSecond.indexOf(".") === -1) {  
+          operandSecond = "0"+`${n}`; 
+        } 
+      } 
+      else {
+        operandSecond += `${n}`;
+      }
+    } else {
+       if (n === ".")  {
+          if (operandSecond.indexOf(".") === -1) { 
+            operandSecond += `${n}`; 
+        } 
+      } 
+      else {
+        operandSecond += `${n}`;
+      }
+    }
+  } 
+
+  else {                                    // цифры вводятся в первый операнд
+    if (operandFirst === "0") {
+      if (n === ".")  {
+         if (operandFirst.indexOf(".") === -1) {  
+          operandFirst = "0"+`${n}`; 
+        } 
+      } 
+      else {
+        operandFirst += `${n}`;
+      }
+    } else {
+       if (n === ".")  {
+          if (operandFirst.indexOf(".") === -1) { 
+            operandFirst += `${n}`; 
+        } 
+      } 
+      else {
+        operandFirst += `${n}`;
+      }
+    }
   }
-}
-function answerResult() {
-  answer = eval(+operandFirst + operator + +operandSecond); // приводим всё к математическому вырожению - вычисляем
-  console.log(answer);
-  displayMemory.value =
-    operandFirst + " " + operator + " " + operandSecond + " = ";
-  display.value = answer;
-  
-  operandFirst = answer;
-  operator = "";
-  operandSecond = "";
-}
-function decimalPress() {}
-addEventListener("keydown", numPress);
-
-//  определённые условия
-classErm.addEventListener("click", erm);
-function erm() {
-  bodyStyle.classList.toggle("true-calc");
-  bodyStyle.classList.toggle("erm-math-class");
-  document.getElementById("clean-erase").classList.toggle("erm-math-class-btn");
-  document.getElementById("clean").classList.toggle("erm-math-class-btn");
-  document.getElementById("divide").classList.toggle("erm-math-class-btn");
-  document.getElementById("multiply").classList.toggle("erm-math-class-btn");
-  document.getElementById("seven").classList.toggle("erm-math-class-btn");
-  document.getElementById("eight").classList.toggle("erm-math-class-btn");
-  document.getElementById("nine").classList.toggle("erm-math-class-btn");
-  document.getElementById("minus").classList.toggle("erm-math-class-btn");
-  document.getElementById("four").classList.toggle("erm-math-class-btn");
-  document.getElementById("five").classList.toggle("erm-math-class-btn");
-  document.getElementById("six").classList.toggle("erm-math-class-btn");
-  document.getElementById("plus").classList.toggle("erm-math-class-btn");
-  document.getElementById("one").classList.toggle("erm-math-class-btn");
-  document.getElementById("two").classList.toggle("erm-math-class-btn");
-  document.getElementById("three").classList.toggle("erm-math-class-btn");
-  document.getElementById("zero").classList.toggle("erm-math-class-btn");
-  document.getElementById("decimal").classList.toggle("erm-math-class-btn");
-  document.getElementById("result").classList.toggle("erm-math-class-btn");
-  document.getElementById("display").classList.toggle("black");
-  document.getElementById("display-memory").classList.toggle("black");
-  document.getElementById("display-result").classList.toggle("black");
-  displayMemory.value = "";
-  display.value = "";
 }
